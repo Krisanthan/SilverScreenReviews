9ee9b52ab8e8b643518e0514890229517a752813
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-02-02 20:59:47 -0500

    intial code for new table column, users membership type

--- SilverScreenReviews/Controllers/UsersController.cs
+++ SilverScreenReviews/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace SilverScreenReviews.Controllers
 {
@@ -24,7 +25,7 @@ namespace SilverScreenReviews.Controllers
         // GET: Users
         public ViewResult Index()
         {
-            var users = _context.Users.ToList();
+            var users = _context.Users.Include(c => c.MembershipType).ToList();
             return View(users);
         }
 
