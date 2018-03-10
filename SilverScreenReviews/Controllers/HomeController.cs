36bcc44c217dcf2c78b2e7967710abe986ff487b
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-03-09 20:13:50 -0500

    added authentication to website

--- SilverScreenReviews/Controllers/HomeController.cs
+++ SilverScreenReviews/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace SilverScreenReviews.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
