545dc5b4fb6d2e5622e0cc56f7b47745dc85fa56
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-03-14 11:11:44 -0400

    created reviews controller and DTO skeleton

---
+++ SilverScreenReviews/Controllers/API/ReviewController.cs
@@ -0,0 +1,24 @@
+﻿using SilverScreenReviews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SilverScreenReviews.Controllers.API
+{
+    public class ReviewController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ReviewController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult CreateNewReview(ReviewDTO reviewDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
