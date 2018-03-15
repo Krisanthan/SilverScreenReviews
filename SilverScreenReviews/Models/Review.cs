66898bd014912b1dcb6e650bdc4d41331e40dbf2
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-03-15 18:54:34 -0400

    created Review class

---
+++ SilverScreenReviews/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SilverScreenReviews.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+        
+        [Required]
+        public Movie Movie { get; set; }
+
+        public int rating { get; set; }
+
+        public DateTime DateReviewed { get; set; }
+    }
+}
